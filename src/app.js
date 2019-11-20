6c292e292350f20f22bfe94d48f19e655a78dce7
srinathsr <srsrinath97@gmail.com>
2019-11-20 19:50:22 +0530

    initial commit

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+class helloworld extends React.Component
+{
+    render() {
+        return (
+
+            <div><h1>helloworld</h1></div>
+        );
+    }
+}
+export default helloworld;
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import helloworld from './Desktop/important/react/myfirst-react-app/src/app';
+ReactDom.render(<helloworld/>,document.getElementById('root'));
